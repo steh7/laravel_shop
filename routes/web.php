c0887b67734f09c851423c4d441de8112357bc11
steh7 <stephanychryscv@gmail.com>
2025-03-13 21:37:09 -0300

    COM FORMS DE LOGIN

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});  
+
+Route::controller(LoginController::class)->group(function() {
+    Route::get('/login', 'index')->name('login');
 });
+
